cf8a38bd329be4f24ee36e6d2853a84d8aad3f96
Pavel Kirpichyov <pavel.kirpichyov@gmail.com>
2012-12-24 15:15:27 +0400

    It's not vendor blob, it can be compiled from sources. Fix compile warning
    Change-Id: I6b9a1ec5853b62df1a3bfb0fba0106b82c16251c

--- qcom-vendor-blobs.mk
+++ qcom-vendor-blobs.mk
@@ -32,7 +32,6 @@ PRODUCT_COPY_FILES += \
     vendor/qcom/msm7x27/proprietary/lib/libsc-a2xx.so:/system/lib/libsc-a2xx.so \
     vendor/qcom/msm7x27/proprietary/lib/egl/eglsubAndroid.so:/system/lib/egl/eglsubAndroid.so \
     vendor/qcom/msm7x27/proprietary/lib/egl/libEGL_adreno200.so:/system/lib/egl/libEGL_adreno200.so \
-    vendor/qcom/msm7x27/proprietary/lib/egl/libGLES_android.so:/system/lib/egl/libGLES_android.so \
     vendor/qcom/msm7x27/proprietary/lib/egl/libGLESv1_CM_adreno200.so:/system/lib/egl/libGLESv1_CM_adreno200.so \
     vendor/qcom/msm7x27/proprietary/lib/egl/libGLESv2_adreno200.so:/system/lib/egl/libGLESv2_adreno200.so \
     vendor/qcom/msm7x27/proprietary/lib/egl/libq3dtools_adreno200.so:/system/lib/egl/libq3dtools_adreno200.so \
